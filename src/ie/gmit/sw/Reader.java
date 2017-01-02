e5d398d5f3e01c60fbcdabe7c6232aaa4e70e60a
Christopher Weir <christopher.weir@hotmail.com>
2017-01-02 13:27:56 +0000

    Created the Reader class
    The Reader class is responsible for reading jar files.

---
+++ src/ie/gmit/sw/Reader.java
@@ -0,0 +1,17 @@
+package ie.gmit.sw;
+
+/**
+ * 
+ * @author Christopher Weir - G00309429
+ * This class is responsible for taking in a jar file and reading its classes.
+ *
+ */
+
+public class Reader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
