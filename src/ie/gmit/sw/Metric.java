0d7cab88f7266fd502e823b990e151b6ec7b8348
Christopher Weir <christopher.weir@hotmail.com>
2017-01-03 15:36:52 +0000

    Created Reflection class

--- src/ie/gmit/sw/Metric.java
+++ src/ie/gmit/sw/Metric.java
@@ -1,11 +1,14 @@
 package ie.gmit.sw;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class Metric {
-	
+
 	private int inDegree;
 	private int outDegree;
-	private Map<String, Metric> graph = new HashMap<>();
+	private Map<String, Metric> graph;
+
+	//public float getStability(){
+
+	//}
 }
--- src/ie/gmit/sw/Reader.java
+++ src/ie/gmit/sw/Reader.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
 
@@ -17,7 +18,9 @@ import com.sun.javafx.collections.MappingChange.Map;
  */
 
 public class Reader {
-	
+
+	List<Class> cls = new ArrayList<Class>();
+
 	public Reader() {}
 
 	/**
@@ -26,14 +29,27 @@ public class Reader {
 	 * @throws IOException
 	 */
 	public void readJarFile(String jarFile) throws FileNotFoundException, IOException{
+
 		JarInputStream in = new JarInputStream(new FileInputStream(new File(jarFile)));
 		JarEntry next = in.getNextJarEntry();
+
 		while (next != null) {
 			if (next.getName().endsWith(".class")) {
 				String name = next.getName().replaceAll("/", "\\.");
 				name = name.replaceAll(".class", "");
 				if (!name.contains("$")) name.substring(0, name.length() - ".class".length());
 				System.out.println(name);
+
+				Class queryClass;
+				try {
+					queryClass = Class.forName(name);
+					cls.add(queryClass);
+					new Reflection(queryClass);
+				} 
+				catch (ClassNotFoundException e) {
+					System.out.println("Couldn't find class '" + name + "'");
+					System.exit(1);
+				} 
 			}
 			next = in.getNextJarEntry();
 		}
